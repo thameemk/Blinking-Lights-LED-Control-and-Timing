3582829fff73122ebf8bf57763c2145af8f8ef58
thameemk612 <thameemk612@gmail.com>
2020-04-22 23:00:00 +0530

    main function added

--- library.c
+++ library.c
@@ -2,15 +2,23 @@
  * BlinkingLED.c
  *
  * Created: 22-04-2020 10.45.40 PM
- * Author : thame
+ * Author : thameem
  */ 
 
 #include <avr/io.h>
-
+#include <util/delay.h>
 
 /* Replace with your library code */
-int myfunc(void)
+int main(void)
 {
+	DDRC = 0xFF; //Output port is C
+	while (1) // helps to make infinite loop
+	{
+		PORTC = 0XFF; //turn on led => logic high
+		_delay_ms(500); //making delay half second
+		PORTC = 0X00; //turn off led =>logic low
+		_delay_ms(1000); //making delay one second
+	}
 	return 0;
 }
 
